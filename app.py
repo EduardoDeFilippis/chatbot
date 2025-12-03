a7f235c680ba3830e819554b017d432220238ac0
Eduardo De Filippis <defilippiseduardo@gmail.com>
2025-12-03 17:01:52 +0100

    Remove unnecessary whitespace in app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import base64
-
-# Recupera il codice criptato dai secrets
 if "HIDDEN_CODE" in st.secrets:
     encrypted_code = st.secrets["HIDDEN_CODE"]
-    
     # Decodifica la stringa da Base64 a Python normale
     decoded_code = base64.b64decode(encrypted_code).decode("utf-8")
-    
-    # Esegui il codice (Magia!)
     exec(decoded_code)
 else:
-    st.error("🔒 Codice sorgente protetto. Impossibile avviare l'applicazione.")
+    st.error("Codice sorgente protetto. Impossibile avviare l'applicazione.")
